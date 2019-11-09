85d13a656c29eb2f108be1736d668a9192f28b11
Itzik Arzoni <itzikiap@gmail.com>
2019-11-09 22:17:33 +0100

    Scene one working with play button

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -1,15 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Drag : MonoBehaviour
-{
-    private void OnMouseDrag()
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{ 
+
+    private RectTransform m_DraggingPlane;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        float leeway = 10;
-        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, leeway);
-        var objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		Debug.Log(name);
 
-        transform.localPosition = objectPosition;
+        m_DraggingPlane = transform as RectTransform;
+
+        SetDraggedPosition(eventData);
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+			Debug.Log("on drag");
+
+            SetDraggedPosition(data);
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        if (data.pointerEnter != null && data.pointerEnter.transform as RectTransform != null)
+            m_DraggingPlane = data.pointerEnter.transform as RectTransform;
+
+		Debug.Log(data);
+
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(m_DraggingPlane, data.position, data.pressEventCamera, out globalMousePos))
+        {
+            transform.position = globalMousePos;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
     }
 }
---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour, IPointerClickHandler
+{
+	public Rigidbody2D ballRigidBody = null ;
+	
+	public void OnPointerClick(PointerEventData eventData) {
+		ballRigidBody.simulated = true;
+		Destroy(gameObject);
+	}
+}
