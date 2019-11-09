4197d96a7a20a1a8744d964ecc2b0ef36e603d6a
igotinfected <igotinfected@gmail.com>
2019-11-09 10:53:31 +0100

    Add basic drag and drop functionality
    Made a script that you can apply to an object with a collider to be able
    to move it around using drag and drop.

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private void OnMouseDrag()
+    {
+        float leeway = 10;
+        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, leeway);
+        var objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.localPosition = objectPosition;
+    }
+}
