6116229a4d57f54ff5efa8bcb329a8be379a4665
igotinfected <igotinfected@gmail.com>
2019-11-09 20:30:24 +0100

    Add basic rotation mechanic
    Currently still affected by drag, trying to get this to work.^

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
+    private const float Distance = 10;
+
     private void OnMouseDrag()
     {
-        float leeway = 10;
-        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, leeway);
+        var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Distance);
         var objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
         transform.localPosition = objectPosition;
