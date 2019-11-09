782dd68fc55fd69e8ffa3da232547c57b505691d
Kwinten Jacobs <kwintenjacobs@gmail.com>
2019-11-09 20:26:21 +0100

    Added victory trigger

---
+++ Assets/Scripts/GameComplete.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameComplete : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "VituvianBall")
+        {
+            Debug.Log("Win!");
+            SceneManager.LoadSceneAsync("Win", new LoadSceneParameters(LoadSceneMode.Additive));
+        }
+    }
+}
