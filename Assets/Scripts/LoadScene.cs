cc524d334ee2f8843025a7f8e680375e287882da
Yann Hoffmann <35955430+Yann21@users.noreply.github.com>
2019-11-09 16:01:44 +0100

    prefab navigation button

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load(string sceneName)
+    {
+        Debug.Log("Loading scene: " + sceneName);
+        SceneManager.LoadSceneAsync(sceneName, new LoadSceneParameters(LoadSceneMode.Single));
+    }
+}
