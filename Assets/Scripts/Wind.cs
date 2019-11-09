e41b808219a4c081e011bc1be132a66979de1272
Kwinten Jacobs <kwintenjacobs@gmail.com>
2019-11-09 23:17:13 +0100

    Added VictoryFrame Prefab + Helicopter wind physics

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public float windForceMultiplier = 10f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Vector2 wind = collision.transform.position - this.transform.position;
+        Debug.Log("Windforce: " + wind);
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(wind);
+    }
+}
