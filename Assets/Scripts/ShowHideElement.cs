d597c88a5ef53b240221954bf051b99531119d62
Kwinten Jacobs <kwintenjacobs@gmail.com>
2019-11-10 13:05:01 +0100

    Main menu + level select

---
+++ Assets/Scripts/ShowHideElement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideElement : MonoBehaviour
+{
+
+    public void ToggleVisibility(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+}
